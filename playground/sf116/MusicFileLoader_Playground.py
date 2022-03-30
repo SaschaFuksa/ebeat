0ba035680faae122711a2d2f3b07bab96e1ce069
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-03-30 20:56:07 +0200

    prepared playground and questions to solve by "trial and error"

---
+++ playground/sf116/MusicFileLoader_Playground.py
@@ -0,0 +1,3 @@
+# Ziel: Musikdatei laden, decodieren -> Tensorflow bietet hierfür Methoden an
+# Decodierte Musikdatei: Was habe ich für eine Datenstruktur in der Hand? Wie erhalte ich eine nummerische Grundlage für das neuronale Netzwerk?
+# Visualisierung: Was kann man aus den decodierten Daten lesen? Ziel: Mustererkennung für stochastische Samples
