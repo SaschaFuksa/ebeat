25805f9525063eecbe18e3513a1ad55f461a42c3
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-04-02 10:17:45 +0200

    Tried code to plot mp3-file and visual data

--- playground/sf116/MusicFileLoader_Playground.py
+++ playground/sf116/MusicFileLoader_Playground.py
@@ -1,3 +1,21 @@
 # Ziel: Musikdatei laden, decodieren -> Tensorflow bietet hierfür Methoden an
 # Decodierte Musikdatei: Was habe ich für eine Datenstruktur in der Hand? Wie erhalte ich eine nummerische Grundlage für das neuronale Netzwerk?
 # Visualisierung: Was kann man aus den decodierten Daten lesen? Ziel: Mustererkennung für stochastische Samples
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_io as tfio
+
+path = 'C:\\Users\\Admin\\Downloads\\Technology Lab\\Music\\Dee Yan-Key - minor melancholy.mp3'
+
+audio = tfio.audio.AudioIOTensor(path)
+print(audio)
+
+audio_slice = audio[100:]
+print(audio_slice)
+
+tensor = tf.cast(audio_slice, tf.float32) / 32768.0
+
+plt.figure()
+plt.plot(tensor.numpy())
+plt.show()
+print(tensor.numpy())
