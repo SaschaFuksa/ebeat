8deac7d4b9afd311d6a4cc438d3da3e0857831d3
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-04-02 15:50:33 +0200

    Created test classes with interface to assert in test class, if result has random length or same length. Test and classes are not implemented so far

---
+++ playground/sf116/RandomMusicFileCutter.py
@@ -0,0 +1,8 @@
+from playground.sf116.CutterConfigurator import CutterConfigurator
+from playground.sf116.IMusicFileCutter import IMusicFileCutter
+
+
+class RandomMusicFileCutter(IMusicFileCutter):
+
+    def cut_music_file(self, cutter_configurator: CutterConfigurator) -> []:
+        pass
