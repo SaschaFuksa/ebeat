4e63030f68b74fae6b5bf49023549a496063bd8b
Nicola Haller <nicolahaller@users.noreply.github.com>
2022-04-07 18:11:31 +0200

    add class MusicFileCollector.py to find all songs. Test method find_songs in class MusicFileCollectorTest.py

---
+++ ebeat_test/audio_sample_cutter_program/MusicFileCollectorTest.py
@@ -0,0 +1,22 @@
+import unittest
+
+from ebeat.audio_sample_cutter_program.MusicFileCollector import MusicFileCollector
+
+
+class MusicFileCollectorTest(unittest.TestCase):
+    path = "test_data"
+
+    def test_find_songs(self):
+        finder = MusicFileCollector(self.path)
+        songs = finder.find_songs()
+        self.assertEqual(len(songs), 4)
+
+    def test_list(self):
+        test_list = ["test_a.mp3", "test_b.mp3", "test_a.wav", "test_b.wav"]
+        finder = MusicFileCollector(self.path)
+        songs = finder.find_songs()
+        self.assertListEqual(test_list, songs, "Lists are equal")
+
+
+if __name__ == '__main__':
+    unittest.main()
