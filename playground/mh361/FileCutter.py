b2ee94575a0ab236038c188e66e13bbbbf8a937a
MichaelHennHDM <mh361@hdm-stuttgart.de>
2022-04-08 09:35:05 +0200

    Updated latest code parts with file loader and file cutter, you can find the full code in FileCutter.py

--- playground/mh361/FileCutter.py
+++ playground/mh361/FileCutter.py
@@ -1,34 +1,61 @@
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from pydub.utils import make_chunks
 import glob
+import os
+import IPython.display as display
+from pydub.playback import play
 
 # Length of the chopped file part in seconds
-length = 5
+size = 6
+
+# Music file directory
 music_dir = 'C:\\Users\\hennm\\Dropbox\\Studium\\Master\\Semester 2\\Tec Lab\\Music\\'
 
+# Get the files within the directory
 wav_filenames = glob.glob(str(music_dir + '*.wav*'))
 wav_file = wav_filenames[0]
-
 mp3_filenames = glob.glob(str(music_dir + '*.mp3*'))
 mp3_file = mp3_filenames[0]
+
+# File Pre-Fix name
+file_prefix = os.path.basename(wav_file).split('.')[0]
 
 # WAV File
 song = AudioSegment.from_wav(wav_file)
 
-# MP3 File
-#song = AudioSegment.from_mp3(mp3_file)
+# Song duration / time to milliseconds/ chunk definition
+song_duration = song.duration_seconds * 1000
+chunk_size_ms = size * 1000
 
-# Other File Formats
-"""
-mp4_version = AudioSegment.from_file("never_gonna_give_you_up.mp4", "mp4")
-wma_version = AudioSegment.from_file("never_gonna_give_you_up.wma", "wma")
-aac_version = AudioSegment.from_file("never_gonna_give_you_up.aiff", "aac")
-"""
+amount_of_chunks = song_duration / chunk_size_ms
 
-# Audio Slicing / second calculation / chunk definition
-song_duration = song.duration_seconds
-seconds = length * 1000
-first_chunk = song[:seconds]
-last_chunk = song[-seconds:]
+# Get first and last chunk -> testing
+first_chunk = song[:chunk_size_ms]
+last_chunk = song[-chunk_size_ms:]
+
+# split audio file based on fix size - seconds
+chunks = make_chunks(song, chunk_size_ms)
+# Loop over fixed chunks and export chunks as wav files
+for i, chunk in enumerate(chunks):
+    chunk_name = file_prefix + " chunk{0}".format(i)  # set the file name + chunk number
+    print(chunk_name)
+    #chunk.export(chunk_name, format="wav") #export the chunks as wav file
+
+# split audio file based on silence
+silence_chunks = split_on_silence(song, min_silence_len=400, silence_thresh=-40)
+# Loop over silence_chunks and export them
+for i, silence in enumerate(silence_chunks):
+    s_chunk_name = file_prefix + " chunk{0}".format(i)  # set the file name + chunk number
+    print(silence.duration_seconds)
+    #silence.export(s_chunk_name, format="wav") #export the chunks as wav file
+
+# Play chunk file
+#play(chunks[0])
+
+# Play chunk in Audio-Widget
+#display.Audio(wav_file, rate=22000, autoplay=False)
+
 
 # Save the file / chunks
 #song.export("chunk.mp3", format="mp3")
--- playground/mh361/FileLoader.py
+++ playground/mh361/FileLoader.py
@@ -6,6 +6,30 @@ import tensorflow as tf
 import tensorflow_io as tfio
 import librosa
 
+music_dir = 'C:\\Users\\hennm\\Dropbox\\Studium\\Master\\Semester 2\\Tec Lab\\Music\\'
+
+wav_filenames = glob.glob(str(music_dir + '*.wav*'))
+wav_file = wav_filenames[0]
+mp3_filenames = glob.glob(str(music_dir + '*.mp3*'))
+mp3_file = mp3_filenames[0]
+
+# File Pre-Fix name
+file_prefix = os.path.basename(wav_file).split('.')[0]
+
+# WAV File
+song = AudioSegment.from_wav(wav_file)
+
+# MP3 File
+#song = AudioSegment.from_mp3(mp3_file)
+
+# Other File Formats
+"""
+mp4_version = AudioSegment.from_file("never_gonna_give_you_up.mp4", "mp4")
+wma_version = AudioSegment.from_file("never_gonna_give_you_up.wma", "wma")
+aac_version = AudioSegment.from_file("never_gonna_give_you_up.aiff", "aac")
+
+# first attempt
+
 dname = 'C:\\Users\\hennm\\Dropbox\\Studium\\Master\\Semester 2\\Tec Lab\\Music\\MP3\\'
 filenames = glob.glob(str(dname+'*.mp3*'))
 sample_file = filenames[1]
@@ -15,7 +39,7 @@ print(audio)
 x , sr = librosa.load(audio)
 #print(type(x), type(sr))#<class 'numpy.ndarray'> <class 'int'>print(x.shape, sr)#(94316,) 2205
 
-"""
+
 audio = tfio.audio.AudioIOTensor(sample_file)
 print(audio)
 
