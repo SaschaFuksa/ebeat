60f149bdd96db983a1b13b37530a6b6ccbf01466
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-04-09 20:31:30 +0200

    Added parameters to test for better reading. Also renamed audData var to audio_data

--- ebeat/music_sample_cutter_program/EqualMusicSampleCutter.py
+++ ebeat/music_sample_cutter_program/EqualMusicSampleCutter.py
@@ -16,14 +16,14 @@ class EqualMusicSampleCutter(MusicSampleCutter):
 
     def cut_music_file(self, music_file_path: str) -> []:
         samples = []
-        rate, audData = scipy.io.wavfile.read(music_file_path)
-        time = len(audData) / rate
+        rate, audio_data = scipy.io.wavfile.read(music_file_path)
+        time = len(audio_data) / rate
         amount_of_samples = time / self.sample_length
         sample_size = int(self.sample_length * rate)
         if amount_of_samples >= 1:
             i = 0
-            while i < len(audData):
-                sample = audData[i:i + sample_size]
+            while i < len(audio_data):
+                sample = audio_data[i:i + sample_size]
                 samples.append(sample)
                 i += sample_size
         return samples
--- ebeat_test/audio_sample_cutter_program/EqualMusicSampleCutterTest.py
+++ ebeat_test/audio_sample_cutter_program/EqualMusicSampleCutterTest.py
@@ -5,14 +5,17 @@ from ebeat.music_sample_cutter_program.EqualMusicSampleCutter import EqualMusicS
 
 class EqualMusicSampleCutterTest(unittest.TestCase):
     path = 'equal_cutter_test_data'
+    predefined_length = 2
+    expected_amount_of_samples = 7
+    rate = 44100
 
-    def test_cut_wav_file(self):
+    def test_cut_equal_sized_file(self):
         file = '/test_file.wav'
-        cutter = EqualMusicSampleCutter(2)
+        cutter = EqualMusicSampleCutter(self.predefined_length)
         samples = cutter.cut_music_file(self.path + file)
-        self.assertEqual(len(samples), 7)
-        sample_length = len(samples[0]) / 44100
-        self.assertEqual(sample_length, 2)
+        self.assertEqual(len(samples), self.expected_amount_of_samples)
+        sample_length = len(samples[0]) / self.rate
+        self.assertEqual(sample_length, self.predefined_length)
 
 
 if __name__ == '__main__':
