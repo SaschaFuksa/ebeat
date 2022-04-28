10bc5f43ae1de35e04f0930cd7c1d76b8514d9d5
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-04-28 14:51:33 +0200

    Some more playground tryings

--- playground/sf116/Step b/TestB.py
+++ playground/sf116/Step b/TestB.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 import pylab
 import tensorflow as tf
+from tensorflow.python.ops.numpy_ops import np_config
 
 import SamplingConstants
+
+np_config.enable_numpy_behavior()
 
 all_samples = os.listdir(SamplingConstants.IN_DIRECTORY)
 
@@ -30,6 +33,14 @@ IMAGE_HEIGHT = 480
 IMAGE_WIDTH = 640
 BATCH_SIZE = 32
 
+image = tf.keras.preprocessing.image_dataset_from_directory(
+    batch_size=BATCH_SIZE,
+    directory=SamplingConstants.IN_SINGLE_SAMPLE_DIRECTORY,
+    shuffle=True,
+    color_mode='rgb',
+    image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+    seed=0)
+
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=BATCH_SIZE,
     directory=SamplingConstants.IN_TRAINING,
@@ -46,3 +57,37 @@ validation_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
     seed=0)
 
+print(validation_dataset)
+
+# Create CNN model
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)))
+model.add(tf.keras.layers.Conv2D(32, 3, strides=2, padding='same', activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(64, 3, padding='same', activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(128, 3, padding='same', activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(4, activation='softmax'))
+
+# Compile model
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.RMSprop(),
+    metrics=['accuracy'],
+)
+
+# Train model for 10 epochs, capture the history
+history = model.fit(train_dataset, epochs=2, validation_data=validation_dataset)
+
+print(model.predict(image))
--- playground/sf116/Step b/TestC.py
+++ playground/sf116/Step b/TestC.py
@@ -1,13 +1,45 @@
 import tensorflow as tf
 from keras import models, layers, Input
+from tensorflow.python.ops.numpy_ops import np_config
+
+np_config.enable_numpy_behavior()
 
 import SamplingConstants
 
 test_file = tf.io.read_file(SamplingConstants.IN_SINGLE_SAMPLE_DIRECTORY + SamplingConstants.IN_SINGLE_FILE_NAME)
 test_audio, _ = tf.audio.decode_wav(contents=test_file)
 
-test_audio = tf.expand_dims(test_audio, axis=2)
+# Another example to expand dims
+# test_audio = tf.expand_dims(test_audio, axis=2)
+test_audio = test_audio.reshape(-1, 441000, 2)
 
+# https://datascience.stackexchange.com/questions/15056/how-to-use-lists-in-tensorflow
 model = models.Sequential()
 model.add(Input(tensor=test_audio))
 model.add(layers.Conv1D(filters=2, kernel_size=2, activation='sigmoid', padding='valid', input_shape=(441000, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling1D(pool_size=(2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv1D(64, 3, padding='same', activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling1D(pool_size=(2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv1D(128, 3, padding='same', activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling1D(pool_size=(2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(4, activation='softmax'))
+
+# Compile model
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.RMSprop(),
+    metrics=['accuracy'],
+)
+
+# Train model for 10 epochs, capture the history
+history = model.fit(test_audio, epochs=3)
