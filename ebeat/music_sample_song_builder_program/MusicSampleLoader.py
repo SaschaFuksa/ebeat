70bada9189eaf4aa1c7b0223814929c1387dfb50
ArlineCarle <ac065@hdm-stuttgart.de>
2022-05-09 17:17:57 +0200

    created MusicSampleLoader.py

--- ebeat/music_sample_song_builder_program/MusicSampleLoader.py
+++ ebeat/music_sample_song_builder_program/MusicSampleLoader.py
@@ -1,6 +1,34 @@
 '''
-Program to load all samples of in_path and prepare/compiles (normalize) data e.g. start/end
+Program to load all samples of in_path and prepare/compiles (normalize) data e.g. start/end 
 '''
+import os
 
-class MusicSampleLoader():
-    pass
+from pydub import AudioSegment
+
+
+class MusicSampleLoader:
+    EDGE_SIZE = 70
+
+    def __init__(self, input_directory: str):
+        self.input_directory = input_directory
+
+    def load_samples(self):
+        start_samples = []
+        end_samples = []
+        files = os.listdir(self.input_directory)
+        files = sorted(files, key=lambda x: int(x.split('_')[-1].split(".")[0]))
+        sample_model = []
+        for file_name in files:
+            if '.wav' in file_name:
+                complete_path = self.input_directory + file_name
+                sample = AudioSegment.from_wav(complete_path)
+                numeric_sample_array = sample.get_array_of_samples()
+                start_samples.append(numeric_sample_array[:self.EDGE_SIZE])
+                end_samples.append(numeric_sample_array[-self.EDGE_SIZE:])
+                model = MusicSampleModel(name=file_name, start=numeric_sample_array[:self.EDGE_SIZE],
+                                         end=numeric_sample_array[-self.EDGE_SIZE:])
+                sample_model.append(model)
+        end_samples = end_samples[:-1]
+        start_samples = start_samples[1:]
+
+        return end_samples, start_samples, sample_model
