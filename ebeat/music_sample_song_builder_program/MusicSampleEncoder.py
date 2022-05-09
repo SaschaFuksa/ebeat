41858937be3500d5dc388fc74ac701c26e0e6fc9
vanessahartmann <vanessaahartmann@googlemail.com>
2022-05-09 17:58:12 +0200

    created MusicSampleEncoder.py

--- ebeat/music_sample_song_builder_program/MusicSampleEncoder.py
+++ ebeat/music_sample_song_builder_program/MusicSampleEncoder.py
@@ -1,3 +1,23 @@
 '''
 Wrapps all encoding model code
 '''
+import numpy
+from keras import Input
+from keras.layers import LSTM
+
+
+class MusicSampleEncoder:
+
+    def __init__(self, source_values_sorted, end_samples):
+        self.num_encoder_tokens = len(source_values_sorted)
+        self.max_encoder_seq_length = max([len(txt) for txt in end_samples])
+        self.encoder_input_data = numpy.zeros(
+            (len(end_samples), self.max_encoder_seq_length, self.num_encoder_tokens),
+            dtype='float32')
+
+    def get_encoder_model(self, latent_dim: int):
+        encoder_inputs = Input(shape=(None, self.num_encoder_tokens))
+        encoder = LSTM(latent_dim, return_state=True)
+        encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+        encoder_states = [state_h, state_c]
+        return encoder_inputs, encoder_states
