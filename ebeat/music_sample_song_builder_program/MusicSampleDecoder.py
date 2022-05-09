bbe5f6077456f56b755ed03e6469f20344ee796b
MichaelHennHDM <mh361@hdm-stuttgart.de>
2022-05-09 21:09:31 +0200

    Added class MusicSampleDecoder.py.

--- ebeat/music_sample_song_builder_program/MusicSampleDecoder.py
+++ ebeat/music_sample_song_builder_program/MusicSampleDecoder.py
@@ -1,66 +1,35 @@
-'''
-Wrapps all decoding model code
-'''
+import numpy 
+ 
+from ebeat.music_sample_song_builder_program.MusicSampleDecoderModel import MusicSampleDecoderModel 
+ 
+ 
+class MusicSampleDecoder: 
+ 
+    def __init__(self, encoder_model, decoder_model, decoder_model_builder: MusicSampleDecoderModel, 
+                 target_token_index): 
+        self.decoder_model = decoder_model 
+        self.encoder_model = encoder_model 
+        self.decoder_model_builder = decoder_model_builder 
+        self.reverse_target_char_index = dict( 
+            (i, float32) for float32, i in target_token_index.items()) 
+ 
+    def decode_sequence(self, input_seq): 
+        states_value = self.encoder_model.predict(input_seq) 
+        target_seq = numpy.zeros((1, 1, self.decoder_model_builder.num_decoder_tokens)) 
+        stop_condition = False 
+        decoded_sentence = [] 
+        while not stop_condition: 
+            output_tokens, h, c = self.decoder_model.predict( 
+                [target_seq] + states_value) 
+            sampled_token_index = numpy.argmax(output_tokens[0, -1, :]) 
+            sampled_char = self.reverse_target_char_index[sampled_token_index] 
+            decoded_sentence.append(sampled_char) 
+            if len(decoded_sentence) >= self.decoder_model_builder.max_decoder_seq_length: 
+                stop_condition = True 
+            target_seq = numpy.zeros((1, 1, self.decoder_model_builder.num_decoder_tokens)) 
+            target_seq[0, 0, sampled_token_index] = 1. 
+            states_value = [h, c] 
+ 
+        return decoded_sentence 
 
-import numpy
-from keras import Input
-from keras.layers import LSTM, Dense
-from keras.models import Model
-
-
-class MusicSampleDecoder:
-    decoder_lstm = None
-    decoder_dense = None
-    decoder_inputs = None
-
-    def __init__(self, target_values_sorted, end_samples):
-        self.num_decoder_tokens = len(target_values_sorted)
-        self.max_decoder_seq_length = max([len(txt) for txt in end_samples])
-
-        self.decoder_input_data = numpy.zeros(
-            (len(end_samples), self.max_decoder_seq_length, self.num_decoder_tokens),
-            dtype='float32')
-        self.decoder_target_data = numpy.zeros(
-            (len(end_samples), self.max_decoder_seq_length, self.num_decoder_tokens),
-            dtype='float32')
-
-    def get_decoder_data(self, latent_dim: int, encoder_states):
-        self.decoder_inputs = Input(shape=(None, self.num_decoder_tokens))
-        self.decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-        decoder_outputs, _, _ = self.decoder_lstm(self.decoder_inputs,
-                                                  initial_state=encoder_states)
-        self.decoder_dense = Dense(self.num_decoder_tokens, activation='softmax')
-        decoder_outputs = self.decoder_dense(decoder_outputs)
-        return self.decoder_inputs, decoder_outputs
-
-    def build_decoder_model(self, latent_dim: int):
-        decoder_state_input_h = Input(shape=(latent_dim,))
-        decoder_state_input_c = Input(shape=(latent_dim,))
-        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-        decoder_outputs, state_h, state_c = self.decoder_lstm(
-            self.decoder_inputs, initial_state=decoder_states_inputs)
-        decoder_states = [state_h, state_c]
-        decoder_outputs = self.decoder_dense(decoder_outputs)
-        decoder_model = Model(
-            [self.decoder_inputs] + decoder_states_inputs,
-            [decoder_outputs] + decoder_states)
-        return decoder_model
-
-    def decode_sequence(self, input_seq):
-        states_value = encoder_model.predict(input_seq)
-        target_seq = numpy.zeros((1, 1, self.num_decoder_tokens))
-        stop_condition = False
-        decoded_sentence = []
-        while not stop_condition:
-            output_tokens, h, c = decoder_model.predict(
-                [target_seq] + states_value)
-            sampled_token_index = numpy.argmax(output_tokens[0, -1, :])
-            sampled_char = reverse_target_char_index[sampled_token_index]
-            decoded_sentence.append(sampled_char)
-            if len(decoded_sentence) >= self.max_decoder_seq_length:
-                stop_condition = True
-            target_seq = numpy.zeros((1, 1, self.num_decoder_tokens))
-            target_seq[0, 0, sampled_token_index] = 1.
-            states_value = [h, c]
-
-        return decoded_sentence
+ 
