1342d89dffd1b832cbc4149486b25a012dfbc21f
SaschaFuksa <sascha.fuksa@gmail.com>
2022-05-09 17:13:20 +0200

    Created music sample model to hold vars: name, start edge and end edge

--- ebeat/music_sample_song_builder_program/MusicSampleModel.py
+++ ebeat/music_sample_song_builder_program/MusicSampleModel.py
@@ -1,3 +1,11 @@
 '''
 Model to hold music file name to end and start edges
 '''
+
+
+class MusicSampleModel:
+
+    def __init__(self, name: str, start: [], end: []):
+        self.end = end
+        self.start = start
+        self.name = name
