9aa08153fc0350131cbd2465053e7c23ba46209f
patrick <treiber_patrick@web.de>
2022-06-02 17:47:54 +0200

    created LSTM to adapt it for our use case

---
+++ playground/pt029/LSTM.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential()
+model.add(layers.LSTM(64, input_shape=(None, 28)))
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(10))
+print(model.summary())
+
+mnist = keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train/255.0, x_test/255.0
+x_validate, y_validate = x_test[:-10], y_test[:-10]
+x_test, y_test = x_test[-10:], y_test[-10:]
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="sgd",
+    metrics=["accuracy"],
+)
+
+model.fit(
+    x_train, y_train, validation_data=(x_test, y_test), batch_size=64, epochs=1
+)
+model.fit(
+    x_train, y_train, validation_data=(x_validate, y_validate), batch_size=64, epochs=10
+)
+
+for i in range(10):
+    result = tf.argmax(model.predict(tf.expand_dims(x_test[i], 0)), axis=1)
+    print(result.numpy(), y_test[i])
